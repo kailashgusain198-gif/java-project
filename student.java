478773c67643350cd55ef77e7a022c31e2a4fd94
kailashgusain198-gif <kailashgusain198@gmail.com>
2026-04-16 13:40:10 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,1 +1,15 @@
+class Student {
+    int studentId;
+    String name;
+    String email;
 
+    public Student(int studentId, String name, String email) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String toString() {
+        return studentId + " - " + name + " - " + email;
+    }
+}
